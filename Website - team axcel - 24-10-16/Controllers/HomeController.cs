2750cad4e5db70991ae6d377b80341e2d608ae80
daniel svanber <daniel.nikten@gmail.com>
2016-10-24 12:31:53 +0200

    Added sveral views

--- Website - team axcel - 24-10-16/Controllers/HomeController.cs
+++ Website - team axcel - 24-10-16/Controllers/HomeController.cs
@@ -13,5 +13,20 @@ namespace Website___team_axcel___24_10_16.Controllers
         {
             return View();
         }
+
+        public ActionResult Purpose()
+        {
+            return View();
+        }
+
+        public ActionResult Members()
+        {
+            return View();
+        }
+
+        public ActionResult Organations()
+        {
+            return View();
+        }
     }
 }
